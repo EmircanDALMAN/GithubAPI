495783f5f758f49ae6b6bda7a8ba1fcea1ba3fed
Kontorta16 <75789956+Kontorta16@users.noreply.github.com>
2021-02-26 19:29:37 +0300

    Completed..

---
+++ app.js
@@ -0,0 +1,53 @@
+const githubForm = document.getElementById("github-form");
+const nameInput = document.getElementById("githubname");
+const clearLastUsers = document.getElementById("clear-last-users");
+const lastUsers = document.getElementById("last-users");
+const github = new Github();
+const ui = new UI();
+
+eventListeners();
+
+function eventListeners() {
+  githubForm.addEventListener("submit", getData);
+  clearLastUsers.addEventListener("click", clearAllSearched);
+  document.addEventListener("DOMContentLoaded", getAllSearched);
+}
+
+function getData(e) {
+  let userName = nameInput.value.trim();
+
+  if (userName === "") {
+    alert("Geçerli Kullanıcı Adı Girin...");
+  } else {
+    github
+      .getGithubData(userName)
+      .then((response) => {
+        if (response.user.message === "Not Found") {
+          ui.showError("Kullanıcı Bulunamadı");
+        } else {
+          ui.addSearchedUserToUI(userName);
+          Storage.addSearchedUserToStorage(userName);
+          ui.showUserInfo(response.user);
+          ui.showRepoInfo(response.repo);
+        }
+      })
+      .catch((err) => ui.showError(err));
+  }
+  ui.clearInput();
+  e.preventDefault();
+}
+function clearAllSearched() {
+    if(confirm("Silmek istediğinizden emin misiniz?")){
+        Storage.clearAllSearchedUsersFromStorage();
+        ui.clearAllSearchedFromUI();
+    }
+
+}
+function getAllSearched() {
+  let users = Storage.getSearchedUsersFromStorage();
+  let result = "";
+  users.forEach((user) => {
+    result += `<li class="list-group-item">${user}</li>`;
+  });
+  lastUsers.innerHTML = result;
+}
